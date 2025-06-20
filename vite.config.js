bd1b3c1642dd2bbcbe463f8bbc8b5a35cba25c20
soomin <wzorba75@gmail.com>
2025-06-20 16:18:49 +0900

    vite.config 재수정

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/prj_TEST/', // base: './', // 상대 경로로 설정 (필수!)  // base: '/prj_TEST/', // 저장소 이름으로 설정 
-  server: {
-    proxy: {
-      //'/api': 'http://localhost:3001/posts'  // /api/posts → 3001/posts로 연결
-      target: 'http://localhost:3001',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ''),
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     //'/api': 'http://localhost:3001/posts'  // /api/posts → 3001/posts로 연결
+  //     target: 'http://localhost:3001',
+  //     changeOrigin: true,
+  //     rewrite: (path) => path.replace(/^\/api/, ''),
+  //   }
+  // }
 })
